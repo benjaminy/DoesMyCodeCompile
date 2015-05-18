da26a7d9e13e58c9c67d94a599e5ff7ad903e9f4
Benjamin Ylvisaker <ben.ylvisaker@coloradocollege.edu>
2015-05-18 13:44:25 -0600

    To home

--- Source/process_build_rules.sh
+++ Source/process_build_rules.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
 RULES_DIR="../BuildRules"
+DEPLOY_DIR="../Deploy"
+ALL_RULES=$DEPLOY_DIR/"build_rules.json"
+
+echo "{" > $ALL_RULES
 
 # WARNING: Spaces in filenames probably creates problems here
 for RULES_FILE in $(find $RULES_DIR -type f); do
+    echo $RULES_FILE
     make -f $RULES_FILE -s tags
     make -f $RULES_FILE -s targets
 done
+
+echo "}" >> $ALL_RULES
